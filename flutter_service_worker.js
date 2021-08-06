84b9d25e227d9e0b134661f51523063c2f9a722b
RubenBarranco <36822421+RubenBarranco@users.noreply.github.com>
2021-08-07 02:05:24 +0900

    and text fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -70,9 +70,9 @@ const RESOURCES = {
 "favicon.ico": "ee63a85ce46c5526467198d28bc6ba7b",
 "icons/Icon-192.png": "750c928436f3d8578e2ab9dd0a9a6a92",
 "icons/Icon-512.png": "edfc0deaab05fc09af6bb4aff04af7e0",
-"index.html": "372efbcea766facd229a5f1efe70ae32",
-"/": "372efbcea766facd229a5f1efe70ae32",
-"main.dart.js": "0b2cff0e52c2a3c5c0c752aa6772bd30",
+"index.html": "2d31c12a399621c02f37449a06d60e2f",
+"/": "2d31c12a399621c02f37449a06d60e2f",
+"main.dart.js": "5a1694a70609d9294ac853f1783df41e",
 "manifest.json": "19193895fec966c8e96550cb5449d997",
 "version.json": "d4095755ca0d9b3ee421f739c79120fe"
 };
